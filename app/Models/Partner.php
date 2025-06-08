064b821a9014b308d4620b7f68d4bbfce0eee11c
rzkyalbani <rzkyalbani@gmail.com>
2025-06-09 01:11:06 +0700

    feat(partner): Create Partner model, migration, and relationships

---
+++ app/Models/Partner.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Partner extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'store_name',
+        'address',
+        'phone_number',
+        'description',
+        'status',
+    ];
+
+    // Relasi: Satu Partner dimiliki oleh satu User
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function partner()
+    {
+        return $this->hasOne(Partner::class);
+    }
 }
