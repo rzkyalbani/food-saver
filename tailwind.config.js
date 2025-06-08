f642ba5a4dae24897cd397a4f4013d477f357004
rzkyalbani <rzkyalbani@gmail.com>
2025-06-09 01:07:45 +0700

    style(auth): Apply custom theme to authentication pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                "primary-green": "#10B981",
+                "dark-green": "#059669",
+                "accent-yellow": "#F59E0B",
+                "bg-white": "#F9FAFB",
+                "text-dark": "#1F2937",
+                "text-gray": "#6B7281",
+                "border-light": "#E5E7EB",
             },
         },
     },
